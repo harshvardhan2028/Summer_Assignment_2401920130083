74ebc4d59c4961309f5e91ffedec00d25b378224
harshvardhan2028 <hvsthakur20@gmail.com>
2026-06-02 16:14:39 +0530

    Implement maxSubArray method for maximum sum

---
+++ Week_1/Day_2/maxSubarray.java
@@ -0,0 +1,16 @@
+package Week_1.Day_2;
+
+public class maxSubarray {
+    public int maxSubArray(int[] nums) { // O(n)
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
+
