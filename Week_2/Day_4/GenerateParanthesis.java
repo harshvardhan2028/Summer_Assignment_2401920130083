2ff2818de1b463670063846882fb43ef86172e91
harshvardhan2028 <hvsthakur20@gmail.com>
2026-06-11 23:44:05 +0530

    Implement GenerateParenthesis class for valid parentheses

---
+++ Week_2/Day_4/GenerateParanthesis.java
@@ -0,0 +1,24 @@
+package Week_2.Day_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParanthesis {
+    public void generate(int n, int l, int r, String s, List<String> ans) {
+        if (r == n) {
+            ans.add(s);
+            return;
+        }
+        if (l < n)
+            generate(n, l + 1, r, s + "(", ans);
+        if (r < l)
+            generate(n, l, r + 1, s + ")", ans);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        generate(n, 0, 0, "", ans);
+        return ans;
+    }
+}
+
