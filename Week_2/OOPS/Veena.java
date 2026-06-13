86229748ab05bb5809c4fdd4ab21330e55a6f87b
harshvardhan2028 <hvsthakur20@gmail.com>
2026-06-13 11:33:25 +0530

    Create Veena.java

---
+++ Week_2/OOPS/Veena.java
@@ -0,0 +1,11 @@
+package OOPS.music.string;
+
+import OOPS.music.Playable;
+
+public class Veena implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
