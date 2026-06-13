cdd414879434c4db968aae159d49da9d68d010a8
harshvardhan2028 <hvsthakur20@gmail.com>
2026-06-13 11:26:41 +0530

    Create StringCompression.java

---
+++ Week_2/Day_5/StringCompression.java
@@ -0,0 +1,27 @@
+package Week_2.Day_5;
+
+public class StringCompression {
+    public int compress(char[] chars) {
+        int write = 0;
+        int read = 0;
+
+        while (read < chars.length) {
+            char curr = chars[read];
+            int count = 0;
+
+            while (read < chars.length && chars[read] == curr) {
+                read++;
+                count++;
+            }
+
+            chars[write++] = curr;
+
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        return write;
+    }
+}
