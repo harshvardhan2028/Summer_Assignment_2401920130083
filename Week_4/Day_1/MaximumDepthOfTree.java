f24426d0de820969865ff2a7a83508de879c3ec2
harshvardhan2028 <hvsthakur20@gmail.com>
2026-06-22 17:04:31 +0530

    Create MaximumDepthOfTree.java

---
+++ Week_4/Day_1/MaximumDepthOfTree.java
@@ -0,0 +1,20 @@
+package Week_4.Day_1;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MaximumDepthOfTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
