28ba5adbe35166b5baff2a56181995ab1c064375
harshvardhan2028 <hvsthakur20@gmail.com>
2026-06-24 22:53:45 +0530

    Implement Lowest Common Ancestor in BST

---
+++ Week_4/Day_3/LowestCommonAncestorInBST.java
@@ -0,0 +1,19 @@
+package Week_4.Day_3;
+
+public class LowestCommonAncestorInBST{
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        while (root != null) {
+
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+        return null;
+    }
+}
